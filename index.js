a42820dd2ffc67b8c66f3ec45942896cc87cde51
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-03-29 22:52:19 +0200

    product-idea view ready

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import {showBuisnessPlan} from './index/business-plan.js';
+import { showProductIdea } from "./index/product-idea.js";
 
 export const main = document.getElementById('main');
 
 const planBtn = document.getElementById('plan');
+const ideaBtn = document.getElementById('idea');
 
 planBtn.addEventListener('click', () => {
-    showBuisnessPlan(main);
+    showBuisnessPlan();
     
+})
+ideaBtn.addEventListener('click', () => {
+    showProductIdea();
 })
 
 
---
+++ index/product-idea.js
@@ -0,0 +1,27 @@
+import { main } from "../index.js";
+
+export function showProductIdea() {
+
+    const image1 = './index/images/pexels-markus-spiske-1089438.jpg';
+    const image2 = './index/images/pexels-ready-made-3850452.jpg';
+
+    main.innerHTML = `<div><h1>Produktidé</h1></div> 
+    <div class="flex.container">
+    <div>
+        <div><img src="${image2}" alt="green phone and computer"></div>
+        <div><p>Min lösning för att uppnå syftet innefattar först och främst ett komplett mjukvarusystem, förpackat i en användarvänlig webb-app med funktionalitet som:</p></div>
+            <ul><li>tillåter individuella mätningar och fakturering. Genom att extrahera schablonkostnaderna för sophantering, el- och vatten-förbrukning i tvättstugan från hyran och i stället fakturera hyresgästen för den verkliga kostnaden anser vi att balansen återställs.</li><br>
+                <li>använder digitala lås och identifiering för åtkomst till tvättstugor och miljöhus.</li><br>
+                <li>i miljöhuset kontrolleras, vägs och registreras avfallet på identifierad hyresgäst som får direkt återkoppling om aktuell kostnad och miljöpåverkan.</li><br>
+                <li>ett bokningssystem för tvättstugor. Bokning av tvättid sker bekvämt hemifrån via en app, vilket förhindrar blockering av tider. Endast vid bokat tid tillåts man tillträde till tvättstugan via den digital identifieringen, hyresgästen är säker på att få behålla sitt bokade pass.</li><br>
+                <li>registrerar förbrukning av el och vatten. För att göra hyresgästen medveten om resursförbrukning och kostnader.</li><br>
+                <li>visa statistik och översikt . Både för hyresgästen och för hyresvärden som ges möjlighet att se den total förbrukning</li><br>
+                <li>möjliggör kommunikation mellan hyresgäst och hyresvärd angående tex felanmälan och driftuppehåll</li><br>
+                <li>implementation av behovsstyrd drift och underhåll. Sensorer i soptunnorna, IoT maskiner i tvättstugor som kan förmedla statusrapporter.</li></ul>
+        </div>  
+    </div>
+    <div><img src="${image1}" alt="digital numbers"></div>
+    <div><p>För att få ut det mesta av systemet förespråkar jag även</p>
+        <ul><li>effektiviserad användning av allmänna utrymmen, eventuellt med en del ombyggnationer. Här finns det stort spelrum att skräddarsy lösningen ( jag ger ett exempel på detta under Affärsplan).</li></ul>
+    </div>`;
+}

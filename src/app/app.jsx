be78f17eab4d4edc4048a85e05387e6dd8f37df9
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-04-14 15:33:35 +0200

    last fix from reverted

---
+++ src/app/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './app.css';
+import Logo from './logo.jsx';
+
+export default class App extends React.Component {
+
+    render() {
+
+        console.log('Inuti App');
+        
+        return(
+            <div>
+                <Logo />
+            </div>
+        );
+    }
+}

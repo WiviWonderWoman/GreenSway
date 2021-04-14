139251d64b6baacf792fe9952d8eb3a9ceb7e496
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-04-14 10:27:38 +0200

    added state conditinal rendering at fraction-button + updated style

--- src/app/fraction-button.jsx
+++ src/app/fraction-button.jsx
@@ -3,15 +3,28 @@ import './app.css';
 
 export default class FractionButton extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {
+            isClicked: false
+        };
+    }
+
     handleClick(key) {
-        console.log('Klick på ' + key );
+        console.log('FractionButtons handleClick. Klick på ' + key );
+
+        this.setState({
+            isClicked: !this.state.isClicked
+        });
     }
 
     render() {
         console.log('Inuti FractionButton');
 
         return (
-            this.props.fractions.map((f) => <li key={f.id} onClick={() => this.handleClick(f.id)}><button className={f.id}>{f.source}</button></li>)
+            this.props.fractions.map((element) => this.state.isClicked === true ? 
+            <li key={element.id} onClick={() => this.handleClick(element.id)}><div className={element.id}><p>Förbrukning</p><p>{element.source}</p></div></li> :  
+            <li key={element.id} onClick={() => this.handleClick(element.id)}><button className={element.id}>{element.source}</button></li>)
         );
     }
 }

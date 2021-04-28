795066647f80f7f2ed167df9901c284ef30d2080
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-04-28 19:39:03 +0200

    added query-parameters to url and reads them in home.jsx

--- src/app/components/home/home.jsx
+++ src/app/components/home/home.jsx
@@ -1,11 +1,41 @@
 import React from "react";
+import Caller from "../../domain/users/caller";
 
 export default class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: ''
+        }
+    }
+
+    getUser(id) {
+        console.log('id: ', id)
+        Caller.get(`/${id}`, {})
+        .then(res => {
+            const user = res.data;
+            console.log('USER: ',user);
+            this.setState({
+                email: user.email
+            })
+            // console.log('OLD state.user: ',this.state.userId);
+            })
+        .catch(error => console.log(error));
+    }
+
+    componentDidMount() {
+        const param = window.location.hash;
+        const splitted = param.split("/");
+        console.log(splitted);
+        this.getUser(splitted[2])
+    }
 
     render() {
         return(
             <div className="user">
-                <h1>Innehåll under utveckling</h1>
+                <h1>Innehåll under utveckling.</h1>
+                <p>Tack för ditt tålamod {this.state.email}</p>
             </div>
         )
     }

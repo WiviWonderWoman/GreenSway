99fff7b76f9adb9b72fae5a248a665b081bb6521
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-05-11 19:30:07 +0200

    added store.js and provider

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./app.css";
 import App from "./components/common/app";
 import { UserServices } from "./services/user-services";
+import store from "./store";
 
 const userServices = new UserServices();
 const user = userServices.checkLocalStorage();
@@ -10,9 +11,13 @@ let username;
 let id;
 
 if (user !== null || undefined) {
-    username =  user.username;
+    username = user.username;
     id = user.id;
 }
 
 let root = document.getElementById("root");
-ReactDOM.render(<App id={id} username={username} userServices={userServices}  />, root); 
+ReactDOM.render(
+    <Provider store={store}>
+        <App id={id} username={username} userServices={userServices} />
+    </Provider>
+    , root);

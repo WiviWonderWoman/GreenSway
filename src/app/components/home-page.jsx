e85078fcd45e71104650163b44f0101bc0785ca4
Wivianne Grapenholt <w.grapenholt@gmail.com>
2021-05-13 20:35:46 +0200

    minor bugg-fix and some text edited

--- src/app/components/home-page.jsx
+++ src/app/components/home-page.jsx
@@ -8,7 +8,6 @@ export default class HomePage extends React.Component {
                 <div className="user">
                     <h1>Innehåll under utveckling.</h1>
                     <h2>Kommande features: </h2>
-                    <p>Kalender för tvättidsbokning</p>
                     <p>Kommunikation med hyresvärd:</p>
                     <p>felanmälan, driftinformation, rapportera status på städning</p>
                 </div>
